87effd12cef7069697e1d90177cea79215149e83
SmirnovLM <smirnoff.2003@yandex.ru>
2024-03-03 21:22:42 +0300

    Revert "commit 1"
    This reverts commit 73aac2395c5ac22c8c151331fd9ab63ffe7d72db.

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,43 +4,17 @@
 
 
 bool checkPrime(uint64_t value) {
-    if (value <= 1)
-        return false;
-    for (uint64_t i = 2; i * i <= value; i++) {
-        if (value % i == 0)
-            return false;
-    }
-    return true;
+// вставьте код функции
 }
 
 uint64_t nPrime(uint64_t n) {
-    uint64_t num = 0;
-    uint64_t res = 0;
-    while (n > 0) {
-        if (checkPrime(num)) {
-            res = num;
-            --n;
-        }
-        ++num;
-    }
-    return res;
+// вставьте код функции
 }
 
 uint64_t nextPrime(uint64_t value) {
-    while (true) {
-        ++value;
-        if (checkPrime(value)) {
-            return value;
-        }
-    }
+// вставьте код функции
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum = 0;
-    for (int i = 0; i < hbound; i++) {
-        if (checkPrime(i)) {
-            sum += i;
-        }
-    }
-    return sum;
+// вставьте код функции
 }
